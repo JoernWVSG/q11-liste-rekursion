657e864edd972eb2fe5e7cba78481947dcb628ae
Jörn Schrader <schrader.joern@schulen.regensburg.de>
2021-10-04 00:08:03 +0200

    Add task c and d

--- Fakultaet.java
+++ Fakultaet.java
@@ -2,12 +2,22 @@ public class Fakultaet {
 
     // Attribute werden nicht benoetigt
     
-    public int f(int n) {
-        if ( n == 1 ) {
+     public int f(int n) {
+        if ( n <= 1 ) {
             return 1;
         } else {
             return n * f(n-1);
         }
+    }
+
+    public int f2(int n) {
+        int f = 1;
+
+        for (int k = 1; k <= n ; k++)
+        {
+            f = f * k ; 
+        }
+        return f;
     }
     
 }
